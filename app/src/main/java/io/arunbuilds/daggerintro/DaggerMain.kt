8d3c2bbf928831451880a0589f561afd9bfe5d6b
Arun sudharsan <sudarun@amazon.com>
2020-04-25 18:06:02 +0530

    Initial commit : Added basic module and basic dependency module.

---
+++ app/src/main/java/io/arunbuilds/daggerintro/DaggerMain.kt
@@ -0,0 +1,11 @@
+package io.arunbuilds.daggerintro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DaggerMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dagger_main)
+    }
+}
---
+++ app/src/main/java/io/arunbuilds/daggerintro/basic_module_dependency/BasicDependencyExample.kt
@@ -0,0 +1,29 @@
+package io.arunbuilds.daggerintro.basic_module_dependency
+
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+
+class Wheel
+class Car(val wheel: Wheel)
+
+@Module
+class WheelModule {
+    @Provides
+    fun providesWheel() = Wheel()
+}
+
+@Module
+class WheeledCarModule {
+    //Note that wheels is not provided explicitly inside the WheeledCarModule
+    //It's resolved at ComponentLevel.
+    @Provides
+    fun providesWheeledCar(wheel: Wheel) = Car(wheel)
+}
+
+@Component(modules = [WheelModule::class, WheeledCarModule::class])
+interface WheeledCarComponent {
+    //since we provided WheelsModule, it'll query WheelModule.providesWheel()
+    //for wheels, and pass it to WheeledCarModule.providesWheeledCar(wheel).
+    fun getMeACar(): Car
+}
